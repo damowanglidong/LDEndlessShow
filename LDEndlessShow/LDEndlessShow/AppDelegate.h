99c689ad4025af083554c749a2bdcaa0068fb2be
wukong <wukonghuaguoshan.com>
2016-03-30 22:17:55 +0800

    创建了无限图片轮播器框架

---
+++ LDEndlessShow/LDEndlessShow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LDEndlessShow
+//
+//  Created by 李东 on 16/3/30.
+//  Copyright © 2016年 XDCodeLee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LDEndlessShow/LDEndlessShow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LDEndlessShow
+//
+//  Created by 李东 on 16/3/30.
+//  Copyright © 2016年 XDCodeLee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
